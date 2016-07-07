f066102ce1fb6244ba67f4a0c867add199b2bae5
aipsteve <steve@atlantisitprofessionals.com>
2016-07-07 00:00:03 -0700

    deleted old commented out code

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -36,21 +36,6 @@ var bio = {
                 var formattedSkill = HTMLskills.replace('%data%', bio.skills[i]);
                 $('#skills').append(formattedSkill);
             })
-
-//            var formattedSkill = HTMLskills.replace('%data%', bio.skills[0]);
-//            $('#skills').append(formattedSkill);
-//            var formattedSkill = HTMLskills.replace('%data%', bio.skills[1]);
-//            $('#skills').append(formattedSkill);
-//            var formattedSkill = HTMLskills.replace('%data%', bio.skills[2]);
-//            $('#skills').append(formattedSkill);
-//            var formattedSkill = HTMLskills.replace('%data%', bio.skills[3]);
-//            $('#skills').append(formattedSkill);
-//            var formattedSkill = HTMLskills.replace('%data%', bio.skills[4]);
-//            $('#skills').append(formattedSkill);
-//            var formattedSkill = HTMLskills.replace('%data%', bio.skills[5]);
-//            $('#skills').append(formattedSkill);
-//            var formattedSkill = HTMLskills.replace('%data%', bio.skills[6]);
-//            $('#skills').append(formattedSkill);
         }
     }
 };
